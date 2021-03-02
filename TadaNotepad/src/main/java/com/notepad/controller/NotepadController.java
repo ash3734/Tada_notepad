8c60a91520b77945f20284bcaef4a069f96e5ed6
anseonghyeon <anseonghyeon@anseongcbookpro>
2021-03-03 07:40:37 +0900

    comment 추

--- TadaNotepad/src/main/java/com/notepad/controller/NotepadController.java
+++ TadaNotepad/src/main/java/com/notepad/controller/NotepadController.java
@@ -18,11 +18,21 @@ public class NotepadController {
 	@Autowired
 	private NotepadService notepadService;
 
+	/* 
+	 * 메모장 리스트 조회
+	 * param : id, create_id, string, created_start, created_end
+	 * return : List 
+	 */
 	@RequestMapping(value = "/notepad/notepadList")
 	public List<Map<String,Object>> notepadList(@RequestParam Map<String,Object> param) throws Exception{
 		return notepadService.notepadList(param);
 	}
-	
+
+	/*
+	 * 메모장 리스트 삽입
+	 * body : create_id, memo
+	 * return : 성공 : suecess, 실패 : error when insert
+	 */
 	@RequestMapping(method = RequestMethod.POST, value = "/notepad/notepadInsert")
 	public String notepadInsert(@RequestBody Map<String,Object> param) throws Exception{
 		int rst = notepadService.notepadInsert(param);
@@ -36,13 +46,18 @@ public class NotepadController {
 		}
 	}
 	
+	/*
+	 * 메모장 리스트 업데이트 
+	 *body : id, memo
+	 *return : 성공 : suecess, 실패 : error when update
+	 */
 	@RequestMapping(method = RequestMethod.PUT, value = "/notepad/notepadUpdate")
 	public String notepadUpdate(@RequestBody Map<String,Object> param) throws Exception{
 		int rst = notepadService.notepadUpdate(param);
 		if(rst==1) {
 			return "suscess";
 		}else if(rst==0){
-			return "error when insert";
+			return "error when update";
 		}else if(rst==-1){
 			return "id is reqired";
 		}else {
@@ -50,7 +65,12 @@ public class NotepadController {
 		}
 		
 	}
-	
+
+	/*
+	 * 메모장 리스트 삭제
+	 * param : id
+	 * return : 성공 : suecess, 실패 : error when delete
+	 */
 	@RequestMapping(method = RequestMethod.DELETE, value = "/notepad/notepadDelete")
 	public String notepadDelete(@RequestParam long id) throws Exception{
 		int rst = notepadService.notepadDelete(id);
